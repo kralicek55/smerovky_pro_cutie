14a35f6be8c813408661a6b29f1e7150008043ae
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-12-27 18:16:35 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        prava_led.showColor(neopixel.colors(NeoPixelColors.Violet))
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xb09eff)
+        basic.pause(500)
+        prava_led.showColor(neopixel.colors(NeoPixelColors.Black))
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x000000)
+        basic.pause(500)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 5; index++) {
+        leva_led.showColor(neopixel.colors(NeoPixelColors.Violet))
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xb09eff)
+        basic.pause(500)
+        leva_led.showColor(neopixel.colors(NeoPixelColors.Black))
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x000000)
+        basic.pause(500)
+    }
+})
+let prava_led: neopixel.Strip = null
+let leva_led: neopixel.Strip = null
+let pasek_led = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
+leva_led = pasek_led.range(0, 1)
+prava_led = pasek_led.range(1, 1)
