b497a9f221c486cef06a6db11f4081c0e1fc9c9d
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-12-27 18:30:59 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 5; index++) {
-        prava_led.showColor(neopixel.colors(NeoPixelColors.Violet))
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x7f00ff)
-        basic.pause(500)
-        prava_led.showColor(neopixel.colors(NeoPixelColors.Black))
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x000000)
-        basic.pause(500)
-    }
-})
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 5; index++) {
         leva_led.showColor(neopixel.colors(NeoPixelColors.Violet))
@@ -18,6 +8,27 @@ input.onButtonPressed(Button.B, function () {
         basic.pause(500)
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    for (let index = 0; index < 5; index++) {
+        l_p_led.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ffff)
+        basic.pause(500)
+        l_p_led.showColor(neopixel.colors(NeoPixelColors.Black))
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x000000)
+        basic.pause(500)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        prava_led.showColor(neopixel.colors(NeoPixelColors.Violet))
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x7f00ff)
+        basic.pause(500)
+        prava_led.showColor(neopixel.colors(NeoPixelColors.Black))
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x000000)
+        basic.pause(500)
+    }
+})
+let l_p_led: neopixel.Strip = null
 let prava_led: neopixel.Strip = null
 let leva_led: neopixel.Strip = null
 basic.showLeds(`
@@ -30,3 +41,4 @@ basic.showLeds(`
 let pasek_led = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
 leva_led = pasek_led.range(0, 1)
 prava_led = pasek_led.range(1, 1)
+l_p_led = pasek_led.range(0, 2)
